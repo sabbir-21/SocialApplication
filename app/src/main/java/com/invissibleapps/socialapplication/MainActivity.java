ae7f5f07d9b85def82217fb91c6bf5a6581459d3
Sabbir <mdsabbirahmed25@gmail.com>
2021-04-02 17:02:31 +0600

    story scroll added

--- app/src/main/java/com/invissibleapps/socialapplication/MainActivity.java
+++ app/src/main/java/com/invissibleapps/socialapplication/MainActivity.java
@@ -19,11 +19,14 @@ public class MainActivity extends AppCompatActivity {
         BottomNavigationView navView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
-        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+
+        //error solved by this 1 and 2 comment
+        //Attempt to invoke virtual method 'void androidx.appcompat.app.ActionBar.setTitle(java.lang.CharSequence)' on a null object reference
+        /*1. AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
                 R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
-                .build();
+                .build(); */
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+       //2. NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
     }
 
